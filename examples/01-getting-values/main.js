458453c213e10ee884006d2ccfad394e572d7620
Tibor Udvari <tibor.udvari@icloud.com>
2018-09-24 11:48:00 +0200

    Echo server

---
+++ examples/01-getting-values/main.js
@@ -0,0 +1,24 @@
+// Connect to server (where to send and receive data from)
+var serverAddress = "https://socket-io-server.glitch.me";
+var socket = io(serverAddress);
+
+// Get UI element references
+var elResult = document.querySelector("#result"); // elResult - element result
+var elButton = document.querySelector("#button");
+var elInput = document.querySelector("#input");
+
+elButton.addEventListener("click", function() { 
+	var data = elInput.value;
+	console.log("Sending " + data + " to server");
+	socket.emit("echo", {"data": data}); // Send data to server
+});
+
+socket.on("echo-callback", function(data) {
+	console.log("Received " + JSON.stringify(data) + " from server");
+	elResult.textContent = data.data;
+});
+
+// Example receive data
+socket.on('hi', (d) => {
+  	console.log("Got message !!!");
+});
